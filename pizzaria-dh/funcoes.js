0dad0cf32bd2e561966af626a2b3491337f48300
Alex <alexrospide@gmail.com>
2021-05-25 19:50:10 -0300

    Incluida função para listar filmes

--- pizzaria-dh/funcoes.js
+++ pizzaria-dh/funcoes.js
@@ -1,1 +1,95 @@
 //arquivo destinado a armazenar as funções do sistema
+const pizzas = [
+    {
+        id: 1,
+        sabor: "Quatro Queijos",
+        categoria: "Salgada",
+        preco: 15.89
+    },
+    {
+        id: 2,
+        sabor: "Morango com Nutella",
+        categoria: "Doce",
+        preco: 30.50
+    },
+    {
+        id: 3,
+        sabor: "Brócolis",
+        categoria: "Vegetariana",
+        preco: 35
+    },
+    {
+        id: 4,
+        sabor: "Lombo Canadense",
+        categoria: "Salgada",
+        preco: 25
+    }
+
+]
+
+//adicionar
+
+function adicionarPizza(sabor,categoria,preco){
+    const pizzaNova = {
+        id: pizzas[pizzas.length-1].id +1,
+        sabor,
+        categoria,
+        preco
+        // sabor: sabor,
+        // categoria: categoria,
+        // preco: preco,
+    }
+    pizzas.push(pizzaNova)
+
+}
+
+//editar
+
+//excluir
+
+//exibirPorID
+
+//exibirTodos
+
+// const listarTodasAsPizzas = () => {
+//     let conteudo = [];
+
+//     //da para criar a função dentro do for each
+//     //ou criar uma funcao fora e chamar no forEach
+//     pizzas.forEach((pizza) => {
+//         conteudo +=  `
+//                     id: ${pizza.id},
+//                     Sabor: ${pizza.sabor}
+//                     Categoria: ${pizza.categoria}
+//                     Preço: ${pizza.preco}
+//                     `
+//     });
+//     return conteudo;
+
+// };
+
+
+function listarTodasAsPizzas(){
+    let conteudo = [];
+
+    //da para criar a função dentro do for each
+    //ou criar uma funcao fora e chamar no forEach
+    pizzas.forEach((pizza) => {
+        conteudo +=  `
+                    id: ${pizza.id},
+                    Sabor: ${pizza.sabor}
+                    Categoria: ${pizza.categoria}
+                    Preço: ${pizza.preco}
+                    `
+    });
+    //return conteudo;
+    console.log(conteudo)
+
+};
+
+module.exports = {
+    listarTodasAsPizzas,
+    adicionarPizza
+    
+
+}
--- pizzaria-dh/index.js
+++ pizzaria-dh/index.js
@@ -1,66 +1,15 @@
+const pizzaria = require('./funcoes.js');
+
 console.log('Pizzaria DH')
 
-const pizzas = [
-    {
-        id: 1,
-        sabor: "Quatro Queijos",
-        categoria: "Salgada",
-        preco: 15.89
-    },
-    {
-        id: 2,
-        sabor: "Morango com Nutella",
-        categoria: "Doce",
-        preco: 30.50
-    },
-    {
-        id: 3,
-        sabor: "Brócolis",
-        categoria: "Vegetariana",
-        preco: 35
-    },
-    {
-        id: 4,
-        sabor: "Lombo Canadense",
-        categoria: "Salgada",
-        preco: 25
-    }
-
-]
-
-const listarTodasAsPizzas = () => {
-    let conteudo = [];
-
-    //da para criar a função dentro do for each
-    //ou criar uma funcao fora e chamar no forEach
-    pizzas.forEach((pizza) => {
-        conteudo +=  `
-                    id: ${pizza.id},
-                    Sabor: ${pizza.sabor}
-                    Categoria: ${pizza.categoria}
-                    Preço: ${pizza.preco}
-                    `
-    });
-    return conteudo;
-
-};
-
-const adicionarPizza = function (sabor,categoria,preco){
-    const pizzaNova = {
-        id: pizzas[pizzas.length-1].id +1,
-        sabor,
-        categoria,
-        preco
-        // sabor: sabor,
-        // categoria: categoria,
-        // preco: preco,
-    }
-    pizzas.push(pizzaNova)
-
-}
-
-adicionarPizza("Marguerita","Salgada",28.20)
-adicionarPizza("Frango com Catupery","Salgada",24.80)
+//console.log(pizzaria.listarTodasAsPizzas())
 
 
-console.log(listarTodasAsPizzas())
+pizzaria.adicionarPizza("Marguerita","Salgada",28.20);
+// pizzariaDH.adicionarPizza("Frango com Catupery","Salgada",24.80);
+
+//console.log(pizzaria.listarTodasAsPizzas())
+pizzaria.listarTodasAsPizzas()
+
+
+
